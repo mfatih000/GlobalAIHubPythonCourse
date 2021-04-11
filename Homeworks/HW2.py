6ce3071c0d476c4d39cf0871c9a4cf0dddcdf1ff
MFatih OĞUL <72889065+mfatih000@users.noreply.github.com>
2021-04-11 13:51:25 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,27 @@
-#Explain your work
+#User Login Application
 
-#Question 1
-for x in range(a):
-	print(a)
+nm0 = (input("Enter Your Nickname(For Login):"))
+pw0 = (input("Enter Your Password(For Login):"))
+
+keys ={"nick_name": nm0 , "password": pw0 }
+pass_counter=5
+while True:
+    nm = (input("Enter Your Nickname(For SignIn) :"))
+    pw = (input("Enter Your Password(For SignIn):"))
+
+
+    if(nm==keys["nick_name"]):
+        if(pw==keys["password"]):
+            print("Welcome")
+            break
+
+
+
+    elif (pass_counter>0 and nm!= "nickname"):
+        print("LOGİN FAİLED")
+        pass_counter -= 1
+        print("Left:",pass_counter)
+
+
+    elif(pass_counter==0):
+        print("Your Account Is Deleted")
