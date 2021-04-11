2329ea320238e910be2f19775e304cdb87458f29
MFatih OĞUL <72889065+mfatih000@users.noreply.github.com>
2021-04-11 13:48:52 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,22 @@
-#Explain your work
+#Day1/Question1
 
-#Question 1
-for x in range(a):
-	print(a)
+list=[1,2,3,4,5,6,7,8]
+half_list1=[]
+half_list2=[]
+for i in range(1,11):
+    if(i<=10/2):
+        half_list2.append(i)
+    else:
+        half_list1.append(i)
+
+changed_list=half_list1+half_list2
+print(changed_list)
+
+
+#Day1/Question2
+
+n=int(input("Write a number:"))
+
+for i in range(0,n+1,2):
+    print(i)
+
